f7a04eef7f1697636f6f4415514e6c183cba0257
Gabriel Chavez <gchavezmardonez@gmail.com>
2023-05-28 21:35:49 -0400

    Update example

--- examples/main.cpp
+++ examples/main.cpp
@@ -2,23 +2,34 @@
 
 Credentials credentials;
 
-const int delayTime = 5000;
+constexpr int delayTime{5000};
 long int pastTime = 0;
 
 void setupCredencials();
 char* readSerial(const char* label);
+
+int option = 2;
 
 void setup() {
     credentials.begin();
     Serial.begin(115200);
     delay(5000);
     Serial.println();
-    setupCredencials();
     pinMode(D3, INPUT_PULLUP);
     pastTime = millis();
 }
 
 void loop() {
+    if (option == 2) {
+        Serial.print(F(("Update Credentials: ")));
+        while(Serial.available() == 0) {
+        }
+        option = Serial.parseInt();
+        Serial.println(option);
+        if (option == 1) {
+            setupCredencials();
+        }    
+    }
     if (millis() - pastTime > delayTime) {
         pastTime = millis();
         Serial.print(F("SSID: "));
@@ -35,20 +46,30 @@ void loop() {
         } else {
             Serial.println(credentials.getUserId());
         }
+        Serial.print(F("Device Id: "));
+        if (strlen(credentials.getDeviceId()) == 0) {
+            Serial.println(F("*Not Set*"));
+        } else {
+            Serial.println(credentials.getDeviceId());
+        }
     }
     if (!digitalRead(D3)) {
         Serial.println(F("Credentials Cleared"));
         credentials.clear();
-        Serial.println(F("Now Restarting"));
+        while (true) {
+            Serial.println(F("Waiting for Restart..."));
+            delay(1000);
+        }
     }
 }
 
 void setupCredencials() {
-    char ssid[WORD_MAX_SIZE];
-    char ssidPassword[WORD_MAX_SIZE];
-    char email[WORD_MAX_SIZE];
-    char emailPassword[WORD_MAX_SIZE];
-    char userId[WORD_MAX_SIZE];
+    char ssid[wordMaxSize];
+    char ssidPassword[wordMaxSize];
+    char email[wordMaxSize];
+    char emailPassword[wordMaxSize];
+    char userId[wordMaxSize];
+    // char deviceId[wordMaxSize];
     strcpy(ssid, readSerial("SSID:"));
     strcpy(ssidPassword, readSerial("SSID Password:"));
     strcpy(email, readSerial("Email:"));
@@ -63,13 +84,17 @@ void setupCredencials() {
         credentials.setWiFiCredentials(ssid, ssidPassword);
         credentials.setAccountCredentials(email, emailPassword, userId);
     }
+    // strcpy(deviceId, readSerial("Device Id:"));
+    // if (strcmp(deviceId, "no") != 0) {
+    //     credentials.setDeviceId(deviceId);
+    // }
 }
 
 char* readSerial(const char* label) {
     Serial.print(label);
     Serial.print(" ");
     bool readData = false;
-    char* data = new char[WORD_MAX_SIZE];
+    char* data = new char[wordMaxSize];
     while (!readData) {
         if (Serial.available() > 0) {
             strcpy(data, Serial.readString().c_str());
